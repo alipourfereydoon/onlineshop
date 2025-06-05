74c5c2fb84965e9d7fb4a04ef397a3050d386a65
ali <ali.pourferydoon1983@gmail.com>
2025-06-05 20:37:35 +0300

    create statics directory includes css, js , img folder and create templates directory includes index.html then adjust views and urls

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #my apps
     'acounts_app',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'statics')]
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
