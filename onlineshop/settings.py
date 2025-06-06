86c1afca6fb8f8e34e9a982cf81d89e50ad873f8
ali <ali.pourferydoon1983@gmail.com>
2025-06-06 10:14:30 +0300

    costomize django user model

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     #my apps
     'acounts_app',
     'home',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "account.User"
