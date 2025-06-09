8daf79c29f26fe7d90edab6d79271691b96a7b51
ali <ali.pourferydoon1983@gmail.com>
2025-06-09 20:56:05 +0300

    create cart applications for management of buying and purches of materials and create templates such as cart_detail.html

--- onlineshop/urls.py
+++ onlineshop/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("home.urls")),
     path('account/',include("account.urls")),
-    path('products/', include("product.urls"))
+    path('products/', include("product.urls")),
+    path('cart/',include('cart.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
